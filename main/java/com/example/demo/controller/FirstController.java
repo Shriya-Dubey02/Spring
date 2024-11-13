2700fedd535e8a050f2e8ea5e9bbd96716fab626
Shriya-Dubey02 <shriyadubey220@gmail.com>
2024-11-13 09:04:21 +0530

    13 NOV

--- main/java/com/example/demo/controller/FirstController.java
+++ main/java/com/example/demo/controller/FirstController.java
@@ -1,6 +1,12 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entity.Student;
@@ -14,7 +20,7 @@ public class FirstController{
 		return "Shriya Dubey";
 	}
 	
-	@GetMapping("demo")
+	@GetMapping("/demo")
 	public int num1()
 	{
 		return 20;
@@ -30,4 +36,33 @@ public class FirstController{
 		s.setName("Nisha");
 		return s;
 	}
+	
+	@GetMapping("/all-students")
+	public List<Student> allStudents(){
+		
+		ArrayList<Student> students= new ArrayList<Student>();
+		students.add(new Student(101, "Nisha", 80));
+		students.add(new Student(102, "Shriya", 70));
+		return students;
+	}
+	
+	
+	// students/anyNumber
+	@GetMapping("/students/{id}")
+	
+	public String learnPathVariable(@PathVariable int id) 
+	{
+		System.out.println(id);
+		return "Data Fetched Successfully";
+	}
+	
+	@PostMapping("/students")
+	public Student addStudent(@RequestBody Student student) 
+	{
+		System.out.println(student);
+		return student;
+		
+	}
+	
+	
 }
--- main/java/com/example/demo/entity/Student.java
+++ main/java/com/example/demo/entity/Student.java
@@ -1,8 +1,12 @@
 package com.example.demo.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Student {
 	
 	private int stud_id;
